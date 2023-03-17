50e7e75e3e1d7cda9021d45263ce8748362d13f5
Kibo <andregaming5@gmail.com>
2023-03-17 19:10:14 +0000

    starting point

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Random rand = new Random();
+
+for (int i = 0; i < 10; i++)
+{
+    int int1 = rand.Next();
+    int int2 = rand.Next();
+
+    if (int1 > int2)
+    {
+        Console.WriteLine($"int1 maior! {int1}");
+    }
+    else
+    {
+        Console.WriteLine($"int2 maior! {int2}");
+    }
+
+    Console.WriteLine($"int1: {int1} int2: {int2}");
+
+    Console.WriteLine("");
+
+}
