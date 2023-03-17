428a4b517d47f11621e520407bb3088afc8afd6b
Kibo <andregaming5@gmail.com>
2023-03-17 20:45:51 +0000

    Mensagem a ser mandada com AppInterval tempo entre cada.

--- Program.cs
+++ Program.cs
@@ -3,16 +3,26 @@
 namespace RSU // Note: actual namespace depends on the project name.
 {
     using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     internal class Program
     {
         static void Main(string[] args)
         {
             var myJsonString = File.ReadAllText("rsu3sinais.json");
-            var myJson = JsonConvert.DeserializeObject(myJsonString);
+            JObject myJson = (JObject) JsonConvert.DeserializeObject(myJsonString);
 
-            //Console.WriteLine(myJsonString);
-            Console.WriteLine(myJson);
+            //getting appInterval
+            int appInterval = (int) myJson["data"]["ITSAPP"]["Facilities"]["AppInterval"];
+
+            Console.WriteLine($"AppInterval : {appInterval} ");
+
+            while (true)
+            {
+                Console.WriteLine("Sending message to server...");
+                Console.WriteLine($"Current time: {DateTime.Now.TimeOfDay}");
+                System.Threading.Thread.Sleep( appInterval );
+            }
         }
     }
 }
