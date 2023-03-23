133ab49c5c8589b1f6e5abafc3c97fd888b5e20b
Diogo Estrela <diogostart@hotmail.com>
2023-03-23 17:55:37 +0000

    Mostrar o Id, escrever timestamp, mostrar timestamp

--- Program.cs
+++ Program.cs
@@ -13,31 +13,42 @@
         {
             try
             {
-                var myJsonString = File.ReadAllText("RSU1.json");
+                string myJsonString = File.ReadAllText("RSU1.json");
                 JSonMessages myJson = JsonConvert.DeserializeObject<JSonMessages>(myJsonString);
 
                 Facilities myFacilities = myJson.data.ITSAPP.facilities;
                 if (myFacilities != null && myFacilities.enabled)
                 {
-                    Thread thr = new Thread(() =>
-                    {
+                    
                         //getting appInterval
                         int appInterval = myFacilities.appinterval;
                         Console.WriteLine($"AppInterval : {appInterval} ");
+                    
                         while (true)
                         {
                             Thread thread = new Thread(() =>
                             {
                                 Console.WriteLine("Sending message to server...");
                                 Console.WriteLine($"Current time: {DateTime.Now.TimeOfDay}");
+                                foreach (var iVIMAPP in myJson.data.ITSAPP.facilities.iVIMAPP)
+                                {
+                                    foreach (var item in iVIMAPP.ivim.ivi)
+                                    {
+                                        Console.WriteLine($"IVI ID:{item.mandatory.iviIdentificationNumber} ");
+                                        item.mandatory.timeStamp = DateTime.UtcNow.Ticks;
+                                        Console.WriteLine($"Timestamp:{item.mandatory.timeStamp} ");
+                                    }
+                                }
+                                //Console.WriteLine($"IVI ID:{myFacilities.iVIMAPP[0].ivim.ivi[0].mandatory.iviIdentificationNumber} ");
+                                //Console.WriteLine($"Timestamp:{myFacilities.iVIMAPP[0].ivim.ivi[0].mandatory.timeStamp} ");
                             });
                             thread.Start();
                             Console.WriteLine($"Current number of threads: {Process.GetCurrentProcess().Threads.Count}");
-                            Thread.Sleep(appInterval);
+                            string jsonStringWrite = JsonConvert.SerializeObject(myJson, Formatting.Indented);
+                            File.WriteAllText("RSU1.json", jsonStringWrite);
+                        Thread.Sleep(appInterval);
                         }
-                    });
-                    thr.Start();
-                }
+                    }
             } catch(Exception e) {
                 Console.WriteLine("Ocorreu uma excepção!!");
                 Console.WriteLine(e.Message);
